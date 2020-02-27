3b1a1c0bfb300ea557186268dbca94c2a9936238
Thisusernamedoesexist <61516184+Thisusernamedoesexist@users.noreply.github.com>
2020-02-27 12:27:55 +0100

    Updated code

--- mario.c
+++ mario.c
@@ -3,31 +3,44 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Operator
 int main(void)
 {
-    int input;
-//    int mellomrom = input;
-    string space =" ";
-    string hash = "#";
-    do
-    {
-        input = get_int("Size: ");
+    int input;              // input from user
+    int controlSpace = -1;  // controls amount of spaces per line
+    bool spaceCheck = true; // checks wether space if loop is allowed to write
+    string space =" ";      // Placeholder for 'space' symbol
+    string hash = "#";      // Placeholder for 'hashtag' symbol
+    
+    // Waiting for user input, repromt user if value below 1 or above 8.
+    do {
+    input = get_int("Size: ");
     }
     while (input < 1 || input > 8);
 
-    if (input > 1 || input < 8){
+    // Initiating loops if input is valid
+    if (input > 1 || input < 8)
+    {
+        // Writes new lines and enables spacecheck for additional loops
         for (int x = 0; x < input; x++)
         {
-            for (int y = 0; y < x+1; y++)
-            {  
-                for (int i = 0; i < input; i++)
+            // Writes hashtags and disables further writing of spaces in between hashtags
+            for (int y = 0; y <= x; y++)
+            {
+                // Writes spaces at the start of each loop equivalent to user input
+                if (spaceCheck == true)
                 {
-                printf("%s", space);
-                }       
-            printf("%s", hash);
-            input = input - 1;
+                    for (int i = 0; i < controlSpace+input; i++)
+                        {
+                        printf("%s", space);
+                        }
+                spaceCheck = false;
+                }
+            printf("%s", hash);     
             }
         printf("\n");
+        spaceCheck = true;
+        controlSpace--;
         }
     }
 }
